da15f4388e69a8d50465699c2ee194b9779dd108
sarveshFlexcar <sarveshflexcar@gmail.com>
2023-07-22 23:16:23 +0530

    Added cart windown and add to cart function

--- Ecommerce/app.js
+++ Ecommerce/app.js
@@ -19,11 +19,11 @@ function addToWishlist(id){
     }
     const wishlistContainer = document.getElementById("mywishlist");
     wishlist.push(id);
-    const b = document.createElement("div");
-    b.className = `card`;
-    b.id= `card-${id}`;
-    b.style = "width: 18rem; margin-left: 50px; margin-bottom: 10px";
-    b.innerHTML=`
+    const product = document.createElement("div");
+    product.className = `card`;
+    product.id= `wishlist-card-${id}`;
+    product.style = "width: 18rem; margin-left: 50px; margin-bottom: 10px";
+    product.innerHTML=`
     <div class="card-body">
         <div style=" display: flex; justify-content: space-around; align-items: center; margin-bottom: 10px;">
             <img src="${items[id].imgsrc}" style="height: 80px; width: 80px" alt="..." />
@@ -37,14 +37,81 @@ function addToWishlist(id){
             <button class="btn btn-danger" onclick="removeFromWishlist(${id})">Remove</button>
         </div>
     </div>`;
-    wishlistContainer.appendChild(b);
+    wishlistContainer.appendChild(product);
 }
 
 function removeFromWishlist(id){
     const wishlistContainer = document.getElementById("mywishlist");
-    const item = document.getElementById(`card-${id}`);
+    const item = document.getElementById(`wishlist-card-${id}`);
     wishlistContainer.removeChild(item);
     wishlist = wishlist.filter(function(ele) {
         return ele !== id;
     })
+}
+
+function addToCart(id){
+    const cartContainer = document.getElementById("mycart");
+    const product = document.createElement("div");
+    product.className = `card`;
+    product.id= `cart-card-${id}`;
+    product.style = "width: 18rem; margin-left: 50px; margin-bottom: 10px";
+    product.innerHTML=`
+        <div class="card-body">
+            <div style=" display: flex; justify-content: space-around; align-items: center; margin-bottom: 10px;">
+                <img src="${items[id].imgsrc}" style="height: 80px; width: 80px" alt="..." />
+                <h5 class="card-title" style="display: inline; margin-left: 50px"> ${items[id].title} </h5>
+            </div>
+            <div class="d-flex justify-content-evenly mb-2">
+                <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                    <button type="button" class="btn"  style="border: black 1px solid" onclick="addItem(${id})">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
+                          </svg>
+                    </button>
+                    <input type="text"  value="1"  id="${product.id}-itemCounter" style="width: 40px; text-align: center;" disabled/>
+                    <button type="button" class="btn" style="border: black 1px solid" onclick="reduceItem(${id})">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-lg" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8Z"/>
+                          </svg>
+                    </button>
+                </div>
+            </div>
+            <hr>
+            <div class="d-flex mb-2 justify-content-around">
+                <div>
+                    Total Price:
+                </div>
+                <div class="d-inline-flex">
+                    <em id="${product.id}-quantity">1</em> x <em id="price">${items[id].price}</em> = <div id="${product.id}-total">$${items[id].price}</div>
+                </div>
+            </div>
+            <div style="display: flex; justify-content: space-around">
+                <button class="btn btn-danger">Remove</button>
+            </div>
+        </div>
+    `;
+    cartContainer.appendChild(product);
+}
+
+function addItem(id){
+    const productId = `cart-card-${id}`;
+    const quantity = document.getElementById(productId+'-quantity');
+    const total = document.getElementById(productId+'-total');
+    const itemCounter = document.getElementById(productId+'-itemCounter');
+    let val = Number.parseInt(itemCounter.getAttribute("value"));
+    itemCounter.setAttribute("value",val+1);
+    quantity.textContent = val+1;
+    total.textContent = "$"+Number.parseInt(price.textContent*Number.parseInt(quantity.textContent));
+}
+
+function reduceItem(id){
+    const productId = `cart-card-${id}`;
+    const quantity = document.getElementById(productId+'-quantity');
+    const total = document.getElementById(productId+'-total');
+    const itemCounter = document.getElementById(productId+'-itemCounter');
+    let val = Number.parseInt(itemCounter.getAttribute("value"));
+    if(val == 0) return;
+    itemCounter.setAttribute("value",val-1);
+    quantity.textContent = val-1;
+    total.textContent = "$"+Number.parseInt(price.textContent*Number.parseInt(quantity.textContent));
 }
