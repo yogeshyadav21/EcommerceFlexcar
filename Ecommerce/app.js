d21e3e30ba96fe5160d110416364e1f85bfaa181
sarveshFlexcar <sarveshflexcar@gmail.com>
2023-07-22 22:28:35 +0530

    added wishlist functionality

--- Ecommerce/app.js
+++ Ecommerce/app.js
@@ -12,23 +12,39 @@ let onlineQueue = [];
 let wishlist = [];
 let cart = [];
 
-console.log(items);
 
 function addToWishlist(id){
+    if(wishlist.indexOf(id) != -1){
+        return;
+    }
     const wishlistContainer = document.getElementById("mywishlist");
+    wishlist.push(id);
     const b = document.createElement("div");
-    b.className = "card";
-    b.style = "width: 18rem; margin-left: 50px;";
+    b.className = `card`;
+    b.id= `card-${id}`;
+    b.style = "width: 18rem; margin-left: 50px; margin-bottom: 10px";
     b.innerHTML=`
     <div class="card-body">
         <div style=" display: flex; justify-content: space-around; align-items: center; margin-bottom: 10px;">
-            <img src="./images/bagus-hernawan-A6JxK37IlPo-unsplash.jpg" style="height: 80px; width: 80px" alt="..." />
-            <h5 class="card-title" style="display: inline; margin-left: 50px"> Iphone X </h5>
+            <img src="${items[id].imgsrc}" style="height: 80px; width: 80px" alt="..." />
+            <h5 class="card-title" style="display: inline; margin-left: 50px"> ${items[id].title} </h5>
         </div>
+        <div style="display: flex; justify-content: center">
+            <h2 class="d-inline">$${items[id].price}</h2>
+          </div>
         <div style="display: flex; justify-content: space-around">
             <button class="btn btn-success">Move to Cart</button>
-            <button class="btn btn-danger">Remove</button>
+            <button class="btn btn-danger" onclick="removeFromWishlist(${id})">Remove</button>
         </div>
     </div>`;
     wishlistContainer.appendChild(b);
+}
+
+function removeFromWishlist(id){
+    const wishlistContainer = document.getElementById("mywishlist");
+    const item = document.getElementById(`card-${id}`);
+    wishlistContainer.removeChild(item);
+    wishlist = wishlist.filter(function(ele) {
+        return ele !== id;
+    })
 }
